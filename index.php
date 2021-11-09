f6135fe563540de7a5b8c1023e657f10247f8c96
fabiogiacomel <fabioceep@gmail.com>
2021-11-08 21:31:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -307,9 +307,9 @@
   </footer><!-- End Footer -->
 
   <!-- Vendor JS Files -->
-  <!--<script src="assets/vendor/aos/aos.js"></script>-->
+  <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!--<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>-->
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
