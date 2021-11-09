8e73316e21ac35a531668887b4f355094de3aefc
fabiogiacomel <fabioceep@gmail.com>
2021-11-08 23:46:16 -0300

    Create consultas.sql

---
+++ inscricao/consultas.sql
@@ -0,0 +1,5 @@
+SELECT curso, COUNT(curso) FROM `inscricoesnovas` GROUP by curso;
+
+SELECT nome FROM `inscricoesnovas` WHERE curso=1
+
+SELECT nome FROM `inscricoesnovas` WHERE curso=1 ORDER BY ASC
