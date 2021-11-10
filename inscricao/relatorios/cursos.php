8c91e2ea5a459e06377e0df482f3f42b69f7919d
fabiogiacomel <fabioceep@gmail.com>
2021-11-09 23:49:47 -0300

    Update cursos.php

--- inscricao/relatorios/cursos.php
+++ inscricao/relatorios/cursos.php
@@ -1,63 +1,104 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
 
-<div class="container mt-3">
-<h2>Relatorios das Inscrições</h2>
-  <p>Quantidade total de Inscritos.</p>       
-<?php
-echo '<table class="table">';
- echo "<thead> <tr><th>Curso</th><th>Inscritos</th></tr></thead>";
+    <div class="container mt-3">
+        <h2>Relatórios das Inscrições</h2>
+        <p>Quantidade total de Inscritos.</p>
+        <?php
+        echo '<table class="table">';
+        echo "<thead> <tr><th>Curso</th><th>Inscritos</th></tr></thead><tbody>";
 
-class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
-    }
+        class TableRows extends RecursiveIteratorIterator
+        {
+            function __construct($it)
+            {
+                parent::__construct($it, self::LEAVES_ONLY);
+            }
 
-    function current() {
-        return "<td>" . parent::current(). "</td>";
-    }
+            function current()
+            {
+                return "<td>" . parent::current() . "</td>";
+            }
 
-    function beginChildren() {
-        echo "<tr>";
-    }
+            function beginChildren()
+            {
+                echo "<tr>";
+            }
 
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
-}
+            function endChildren()
+            {
+                echo "</tr>" . "\n";
+            }
+        }
 
-$servername = "localhost";
-$username = "u224722929_ceep";
-$password = "UmhNWJ3AvJ4+H]Kr";
-$dbname = "u224722929_ceep";
+        $servername = "localhost";
+        $username = "u224722929_ceep";
+        $password = "UmhNWJ3AvJ4+H]Kr";
+        $dbname = "u224722929_ceep";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM InscritosCurso");
-    $stmt->execute();
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * FROM InscritosCurso");
+            $stmt->execute();
 
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-        echo $v;
-    }
-}
-catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-$conn = null;
-echo "</table>";
-?>
+            foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
+                echo $v;
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        echo "</tbody></table>";
+        ?>
 
+
+        <!--*************************************************************************************-->
+
+
+
+        <p>Quantidade total de Inscritos Integrado.</p>
+        <?php
+        echo '<table class="table">';
+        echo "<thead> <tr><th>Curso</th><th>Inscritos</th></tr></thead><tbody>";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * FROM InscritosIntegrado");
+            $stmt->execute();
+
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
+                echo $v;
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        echo "</tbody></table>";
+        ?>
+
+        <!--*************************************************************************************-->
+
+
+
+    </div>
 </body>
+
 </html>
