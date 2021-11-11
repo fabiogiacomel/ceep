3008c2669e71e3712cab2ce87c8d98b11b4ed0ab
fabiogiacomel <fabioceep@gmail.com>
2021-11-11 17:45:09 -0300

    Update cursos.php

--- inscricao/relatorios/cursos.php
+++ inscricao/relatorios/cursos.php
@@ -1,3 +1,33 @@
+<?php
+       class TableRows extends RecursiveIteratorIterator
+       {
+           function __construct($it)
+           {
+               parent::__construct($it, self::LEAVES_ONLY);
+           }
+
+           function current()
+           {
+               return "<td>" . parent::current() . "</td>";
+           }
+
+           function beginChildren()
+           {
+               echo "<tr>";
+           }
+
+           function endChildren()
+           {
+               echo "</tr>" . "\n";
+           }
+       }
+
+       $servername = "localhost";
+       $username = "u224722929_ceep";
+       $password = "UmhNWJ3AvJ4+H]Kr";
+       $dbname = "u224722929_ceep";
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -17,33 +47,7 @@
         echo '<table class="table">';
         echo "<thead> <tr><th>Curso</th><th>Inscritos</th></tr></thead><tbody>";
 
-        class TableRows extends RecursiveIteratorIterator
-        {
-            function __construct($it)
-            {
-                parent::__construct($it, self::LEAVES_ONLY);
-            }
-
-            function current()
-            {
-                return "<td>" . parent::current() . "</td>";
-            }
-
-            function beginChildren()
-            {
-                echo "<tr>";
-            }
-
-            function endChildren()
-            {
-                echo "</tr>" . "\n";
-            }
-        }
-
-        $servername = "localhost";
-        $username = "u224722929_ceep";
-        $password = "UmhNWJ3AvJ4+H]Kr";
-        $dbname = "u224722929_ceep";
+ 
 
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
