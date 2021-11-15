1ab5552c2d849755c863c6626e28b81deb390038
fabiogiacomel <fabioceep@gmail.com>
2021-11-15 11:57:46 -0300

    Update lerJson.php

--- inscricao/relatorios/lerJson.php
+++ inscricao/relatorios/lerJson.php
@@ -1,7 +1,7 @@
 <?php
 $json = file_get_contents("inscricoesnovas.json");
 
-$arr = json_decode($jsonobj, true);
+$arr = json_decode($json, true);
 
 foreach($arr as $key => $value) {
   echo $key . " => " . $value . "<br>";
