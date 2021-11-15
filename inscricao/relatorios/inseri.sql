5cac9ff8e9ce51ab75f331f2c9a0dcf781dc7e0d
fabiogiacomel <fabioceep@gmail.com>
2021-11-15 11:10:49 -0300

    inserir

---
+++ inscricao/relatorios/inseri.sql
@@ -0,0 +1,14 @@
+INSERT INTO  inscricoesValidas  ( id ,  nome ,  nome_mae ,  escola_origem ,  escola_origem_cidade ,  escola_origem_estado ,
+  pontuacao ,  rg ,  fonecasa ,  fonecelular ,  fonecomercial ,  fonerecado ,  email ,  bairro ,  cidade ,  curso ,  curso2 , 
+  periodo ,  periodo2 ,  serial ,  ensinofundamental ,  ensinomedio ,  cpf ,  datainsc ,  dataalteracao ,  horainsc , 
+  abandono ,  medio1 ,  medio2 ,  medio3 ,  maiorport ,  maiormat ,  maiorqui ,  maiorbio ,  renda ,  comprovante_renda , 
+  tentativas ,  cursosfeitos ,  auxiliarenf ,  trabalhaarea ,  uf ,  dddcasa ,  dddcelular ,  dddcomercial ,  dddrecado ,
+   nomerecado ,  datanasc ,  rua ,  num_casa ,  cgm ,  concordo ,  instituicao_formacao ,  trabalha_area ,  tipo_necessidade ,
+    bolsa_familia ,  matematica_n1 ,  matematica_n2 ,  matematica_n3 ,  matematica_n4 ,  portugues_n1 ,  portugues_n2 , 
+    portugues_n3 ,  portugues_n4 ,  quimica_n1 ,  quimica_n2 ,  quimica_n3 ,  quimica_n4 ,  biologia_n1 ,  biologia_n2 ,
+     biologia_n3 ,  biologia_n4 ,  nis ,  rg_data_expedicao ,  pontuacao_curso2 ,  valida ) 
+    VALUES ('', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
+    '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
+    '0', '0', '0', '0', '0');
