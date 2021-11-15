811021deb12a9a3da2f2224524d0e668b331bfe7
fabiogiacomel <fabioceep@gmail.com>
2021-11-15 12:27:08 -0300

    Update lerJson.php

--- inscricao/relatorios/lerJson.php
+++ inscricao/relatorios/lerJson.php
@@ -3,10 +3,10 @@ $json_str = file_get_contents("inscricoesnovas.json");
 
 //faz o parsing da string, criando o array "empregados"
 $jsonObj = json_decode($json_str);
-$inscritos = $jsonObj->inscritos;
+$inscricoes = $jsonObj->inscricoes;
 
 //navega pelos elementos do array, imprimindo cada empregado
-foreach ( $inscritos as $i )
+foreach ( $inscricoes as $i )
     {
 	echo "nome: $i->nome - idade: $i->cpf - sexo: $i->curso<br>";
     }
