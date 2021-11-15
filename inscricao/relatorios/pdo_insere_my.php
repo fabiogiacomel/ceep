efa45e334d100403664bd050018397e129ee09cb
fabiogiacomel <fabioceep@gmail.com>
2021-11-15 11:34:54 -0300

    Update pdo_insere_my.php

--- inscricao/relatorios/pdo_insere_my.php
+++ inscricao/relatorios/pdo_insere_my.php
@@ -18,7 +18,7 @@ try
       );
 
 
-      $sql = "INSERT INTO  inscricoesValidas  (nome, nome_mae, escola_origem, escola_origem_cidade, escola_origem_estado,
+      $sql = "INSERT INTO  inscricoesValidas  (id, nome, nome_mae, escola_origem, escola_origem_cidade, escola_origem_estado,
       pontuacao, rg, fonecasa, fonecelular, fonecomercial, fonerecado, email, bairro, cidade, curso, curso2, periodo,
       periodo2, 'serial', ensinofundamental, ensinomedio, cpf, datainsc, dataalteracao, horainsc, abandono,  
       medio1, medio2, medio3, maiorport, maiormat, maiorqui, maiorbio, renda, comprovante_renda, tentativas,
@@ -27,7 +27,7 @@ try
       matematica_n1, matematica_n2, matematica_n3, matematica_n4, portugues_n1, portugues_n2, portugues_n3,
       portugues_n4, quimica_n1, quimica_n2, quimica_n3, quimica_n4, biologia_n1, biologia_n2, biologia_n3,
       biologia_n4, nis, rg_data_expedicao, pontuacao_curso2, valida)
-      VALUES ($dados[1][1], '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
+      VALUES (null, $dados[1][1], '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
         '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
         '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
         '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
