1f9eea164daf34aded3b1b5ed63ab143bcab2201
fabiogiacomel <fabioceep@gmail.com>
2021-11-23 15:15:04 -0300

    novo menu

--- index.php
+++ index.php
@@ -50,28 +50,41 @@
       </div>
     </div>
   </section><!-- End Top Bar-->
-
   <!-- ======= Header ======= -->
   <header id="header" class="d-flex align-items-center">
-    <div class="container d-flex justify-content-between align-items-center">
-      
+    <div class="container d-flex justify-content-between">
       <div id="logo">
         <h1><a href="index.php">CEE<span>P</span></a></h1>
-        <nav id="navbar" class="navbar">
-          <li class="dropdown"><a href="#"><span>Cursos</span> <i class="bi bi-chevron-down"></i></a></li>
-              <li><a href="adm.html">Administração</a></li>
-              <li><a href="edf.html">Edificações</a></li>
-              <li><a href="elm.html">Eletromecânica</a></li>
-              <li><a href="elt.html">Eletrônica</a></li>
-              <li><a href="enf.html">Enfermagem</a></li>
-              <li><a href="inf.html">Desenvolvimen. Sistemas</a></li>
-              <li><a href="amb.html">Meio Ambiente</a></li>
-              <li><a href="seg.html">Segurança do trabalho</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Destaques</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contato</a></li>
+      </div>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
-      </div>
+
     </div>
   </header><!-- End Header -->
 
