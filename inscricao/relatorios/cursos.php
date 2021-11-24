27f7a9a146206921e692119ebdc1e25cf251ad9e
fabiogiacomel <fabioceep@gmail.com>
2021-11-24 11:46:26 -0300

    Update cursos.php

--- inscricao/relatorios/cursos.php
+++ inscricao/relatorios/cursos.php
@@ -182,7 +182,7 @@
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare("SELECT * FROM InscritosSubM");
+            $stmt = $conn->prepare("SELECT * FROM InscritosSubN");
             $stmt->execute();
 
             // set the resulting array to associative
@@ -197,7 +197,7 @@
         $conn = null;
         echo "</tbody></table>";
         ?>
-        
+
         <!--*************************************************************************************-->
 
     </div>
