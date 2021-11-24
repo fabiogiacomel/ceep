533741ed16c23b163762c4130ff0e41e97f24a18
fabiogiacomel <fabioceep@gmail.com>
2021-11-23 21:43:48 -0300

    Update lista.php

--- inscricao/relatorios/lista.php
+++ inscricao/relatorios/lista.php
@@ -5,20 +5,23 @@
        $dbname = "u224722929_ceep";
 try
 {
-    // instancia objeto PDO, conectando no Postgresql
     $conn = new PDO('mysql:dbname=u224722929_ceep;user=u224722929_ceep;password=UmhNWJ3AvJ4+H]Kr;host=localhost');
-    
-    // executa uma instru��o SQL de consulta
-    $result = $conn->query("SELECT DISTINCT cpf  FROM inscricoesValidas");
+    $result = $conn->query("SELECT DISTINCT cpf FROM inscricoesValidas");
     if ($result)
     {
-        // percorre os resultados via fetch()
         while ($row = $result->fetch(PDO::FETCH_OBJ))
         {
-            echo $row->cpf . "</br>";
+        
+            $result = $conn->query("SELECT DISTINCT COUNT(cpf) AS total FROM inscricoesValidas WHERE cpf=$row->cpf");
+            if ($result)
+                {
+                    while ($row = $result->fetch(PDO::FETCH_OBJ))
+                        {
+                            echo $row->total . "</br>";
+                        }
+                }
         }
     }
-    // fecha a conex�o
     $conn = null;
 }
 catch (PDOException $e)
@@ -26,4 +29,3 @@ catch (PDOException $e)
     print "Erro!: " . $e->getMessage() . "<br/>";
     die();
 }
-?>
