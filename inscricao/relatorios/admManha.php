310c1cae6d0943f16f010dcb405004a6be2cc9d0
fabiogiacomel <fabioceep@gmail.com>
2021-11-30 15:46:13 -0300

    Update admManha.php

--- inscricao/relatorios/admManha.php
+++ inscricao/relatorios/admManha.php
@@ -51,7 +51,7 @@
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare("SELECT nome, pontuacao FROM inscricoesValidas WHERE valida=1 AND curso=1 AND periodo = 1 ORDER by pontuacao DESC");
+            $stmt = $conn->prepare("SELECT nome, cast(pontuacao as unsigned integer) as pontuacao FROM inscricoesValidas WHERE valida=1 AND curso=1 AND periodo = 1 ORDER by pontuacao DESC LIMIT 80");
             $stmt->execute();
 
             // set the resulting array to associative
