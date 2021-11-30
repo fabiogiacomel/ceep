ffd8b24be2d5f924d308aefbfa0cad6bee5c41f9
fabiogiacomel <fabioceep@gmail.com>
2021-11-30 16:50:24 -0300

    Update admManhaT.php

--- inscricao/relatorios/admManhaT.php
+++ inscricao/relatorios/admManhaT.php
@@ -1,8 +1,8 @@
 <?php
-       $servername = "localhost";
-       $username = "u224722929_ceep";
-       $password = "UmhNWJ3AvJ4+H]Kr";
-       $dbname = "u224722929_ceep";
+$servername = "localhost";
+$username = "u224722929_ceep";
+$password = "UmhNWJ3AvJ4+H]Kr";
+$dbname = "u224722929_ceep";
 ?>
 
 <!DOCTYPE html>
@@ -17,35 +17,45 @@
 </head>
 
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <img src="https://www.ceepcascavel.com.br/img/ceep.jpg" class="img-fluid rounded"> 
+        </div>
+            <div class="col">
+            <h1 class="text-center"> Administração - Manhã </h1>
+                    </div>
+        </div>
+        
+        
 
-    <div class="container mt-3">
-        <img src="https://www.ceepcascavel.com.br/img/ceep.jpg" class="img-fluid rounded"> 
-        <h1 class="text-center"> Administração - Manhã </h1>
-        <?php
-        echo '<table class="table table-striped">';
-        echo "<thead> <tr> <th>Classificação</th><th>Nome</th><th>Pontuação</th></tr></thead><tbody>";
+        <div class="container mt-3">
 
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare("SELECT nome, cast(pontuacao as unsigned integer) as pontuacao FROM inscricoesValidas WHERE valida=1 AND curso=1 AND periodo = 1 ORDER by pontuacao DESC LIMIT 80");
-            $stmt->execute();
+            <?php
+            echo '<table class="table table-striped">';
+            echo "<thead> <tr> <th>Classificação</th><th>Nome</th><th>Pontuação</th></tr></thead><tbody>";
 
-            // set the resulting array to associative
-            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            $i=1;
-            foreach (new RecursiveArrayIterator($stmt->fetchAll()) as $k => $v) {               
-                echo "<tr><td>$i</td><td class=\"text-uppercase\">".$v["nome"]."</td><td>".$v["pontuacao"]."</td></tr>";
-                $i++;
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $stmt = $conn->prepare("SELECT nome, cast(pontuacao as unsigned integer) as pontuacao FROM inscricoesValidas WHERE valida=1 AND curso=1 AND periodo = 1 ORDER by pontuacao DESC LIMIT 80");
+                $stmt->execute();
+
+                // set the resulting array to associative
+                $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                $i = 1;
+                foreach (new RecursiveArrayIterator($stmt->fetchAll()) as $k => $v) {
+                    echo "<tr><td>$i</td><td class=\"text-uppercase\">" . $v["nome"] . "</td><td>" . $v["pontuacao"] . "</td></tr>";
+                    $i++;
+                }
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
             }
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-        $conn = null;
-        echo "</tbody></table>";
-        ?>
+            $conn = null;
+            echo "</tbody></table>";
+            ?>
 
-    </div>
+        </div>
 </body>
 
 </html>
