bcef266f5d9c13fc0ddcae89a1bc0787cc4ac9f5
fabiogiacomel <fabioceep@gmail.com>
2021-12-01 19:32:41 -0300

    Update FormEntrevista.php

--- entrevista/FormEntrevista.php
+++ entrevista/FormEntrevista.php
@@ -2,7 +2,7 @@
   <div class="form-group row">
     <label for="staticEmail" class="col-sm-2 col-form-label">Nome</label>
     <div class="col-sm-10">
-	<input type="text" readonly class="form-control-plaintext" name="idInscricao" id="idInscricao" value="<?php echo $id;?>">
+	<input type="text" readonly class="form-control-plaintext" name="idInscricao" id="idInscricao" value="<?php echo $idInscricao;?>">
       <input type="text" readonly class="form-control-plaintext" name="nome" id="nome" value="<?php echo $nome;?>">
     </div>
   </div>
