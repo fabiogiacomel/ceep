e3b83d5583f903662043b4f2b3cd61c177ab8cc0
fabiogiacomel <fabioceep@gmail.com>
2021-12-01 16:24:12 -0300

    en

---
+++ entrevista/FormEntrevistaDados.php
@@ -0,0 +1,4 @@
+		echo '<p class="lead">CPF: ' .$cpf. '</p>';
+			echo '<p class="lead">Nome: ' .$nome. '</p>';
+			echo '<p class="lead">Curso Técnico em ' .$cursoNome. '</p>';
+			echo '<p class="lead">Peridodo ' .$periodoNome. '</p>';
--- entrevista/entrevista.php
+++ entrevista/entrevista.php
@@ -138,11 +138,6 @@ switch ($curso) {
   		<button type="submit" class="btn btn-primary">Começar</button>
 		</form>';
 
-		}else{
-			echo '<p class="lead">CPF: ' .$cpf. '</p>';
-			echo '<p class="lead">Nome: ' .$nome. '</p>';
-			echo '<p class="lead">Curso Técnico em ' .$cursoNome. '</p>';
-			echo '<p class="lead">Peridodo ' .$periodoNome. '</p>';
 		}
   		?>
 	</div>
