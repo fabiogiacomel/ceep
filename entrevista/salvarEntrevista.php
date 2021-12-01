148706ebbf37a598b37c037f1f42967b9e96423d
fabiogiacomel <fabioceep@gmail.com>
2021-12-01 18:18:03 -0300

    Update salvarEntrevista.php

--- entrevista/salvarEntrevista.php
+++ entrevista/salvarEntrevista.php
@@ -111,8 +111,8 @@ $dbname = "u224722929_ceep";
          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		// prepare sql and bind parameters
-  		$stmt = $conn->prepare("INSERT INTO tbEntrevistaRespostas (id, cpf, nome, curso, periodo, p1, p2, p3, p4, p5) 
-		VALUES (:id, :cpf, :nome, :curso, :periodo, :p1, :p2, :p3, :p4, :p5)");
+  		$stmt = $conn->prepare("INSERT INTO tbEntrevistaRespostas (id, cpf, nome, curso, periodo, p1, p2, p3, p4, p5, p6) 
+		VALUES (:id, :cpf, :nome, :curso, :periodo, :p1, :p2, :p3, :p4, :p5, :p6)");
   		$stmt->bindParam(':id', $id);
   		$stmt->bindParam(':cpf', $cpf);
 		$stmt->bindParam(':nome', $nome);
@@ -123,7 +123,7 @@ $dbname = "u224722929_ceep";
   		$stmt->bindParam(':p3', $p3);
   		$stmt->bindParam(':p4', $p4);
   		$stmt->bindParam(':p5', $p5);
-  		//$stmt->bindParam(':p6', $p6);
+  		$stmt->bindParam(':p6', $p6);
   		//$stmt->bindParam(':p7', $p7);
   		//$stmt->bindParam(':p8', $p8);
   		//$stmt->bindParam(':p9', $p9);
