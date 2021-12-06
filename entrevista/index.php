a53a9d0894d813a7cd3c48470553d0c27470ce3e
fabiogiacomel <fabioceep@gmail.com>
2021-12-06 05:46:32 -0300

    entrevistas encerradas

--- entrevista/index.php
+++ entrevista/index.php
@@ -31,7 +31,7 @@
 
 	<section>
   <div class="jumbotron text-center">
-    	<p> <strong>ATENÇÃO!</strong><br></p>
+    	<p> <strong>ENTREVISTAS ENCERRADAS!</strong><br></p>
   		<p>A entrevista on-line é para os candidatos que o curso teve mais de 40 inscritos. De cada curso foram classificados 80 candidatos. Para o curso de <strong>ADMINISTRAÇÃO</strong> do período da <strong>MANHÃ</strong> são 120 classificados. Por que temos 80 vagas, portanto até o candidato 120º deve fazer a entrevista.</p>
   		<p>Os candidatos classificados devem fazer a entrevista através de um <strong>FORMULÁRIO (via site do CEEP) </strong>. O Formulário On-Line ficará disponível até o dia 05/12/2021 às 23h59 <br>
   </div>
@@ -40,92 +40,7 @@
    	</div>
 	</div>
 </section>
-<section>
 
-	
-	<form action="salvarEntrevista.php" method="post">
-<div class="form-group">
-    <label for="nome">Nome</label>
-    <input class="form-control" name="nome" id="nome"></input>
-  </div>
-
-  <div class="form-group">
-    <label for="cpf">CPF</label>
-    <input class="form-control" name="cpf" id="cpf"></input>
-  </div>
-
-  <div class="form-group">
-    <label for="curso">Curso</label>
-    <input class="form-control" name="curso" id="curso"></input>
-  </div>
-
-  <div class="form-group">
-    <label for="periodo">Periodo</label>
-    <input class="form-control" name="periodo" id="periodo"></input>
-  </div>
-
-
-<div class="form-group">
-    <label for="P1">Cite sua maior facilidade e a sua maior dificuldade nos estudos e justifique.</label>
-    <textarea class="form-control" name="P1" id="P1" rows="3"></textarea>
-  </div>
-<div class="form-group">
-    <label for="P2">Cite em ordem de preferência três profissões que você deseja exercer no futuro.</label>
-    <textarea class="form-control" name="P2" id="P2" rows="3"></textarea>
-  </div>
-	<div class="form-group">
-    <label for="P3">Cite o principal motivo de escolha deste curso.</label>
-    <textarea class="form-control" name="P3" id="P3" rows="3"></textarea>
-  </div>
-	
-		<div class="form-group">
-    <label for="P4">Comente se há campo de trabalho na sua região para atuação profissional de acordo com o curso escolhido.</label>
-    <textarea class="form-control" name="P4" id="P4" rows="3"></textarea>
-  </div>
-	
-		<div class="form-group">
-    <label for="P5">Cite uma empresa localizada na sua região/cidade que contrata profissionais formados na área do curso que você escolheu.</label>
-    <textarea class="form-control" name="P5" id="P5" rows="3"></textarea>
-  </div>
-	
-		<div class="form-group">
-    <label for="P6">Você já conversou com algum profissional que atua na área do curso que você escolheu para saber como são as atividades desenvolvidas no dia a dia?
-</label>
-    <textarea class="form-control" name="P6" id="P6" rows="3"></textarea>
-  </div>
-	
-		<div class="form-group">
-    <label for="P7">O que você espera do ensino técnico para a sua formação profissional?
-</label>
-    <textarea class="form-control" name="P7" id="P7" rows="3"></textarea>
-  </div>
-	
-	<div class="form-group">
-    <label for="P8">Cite três contribuições que o curso escolhido poderá trazer à sua comunidade/região/cidade.</label>
-    <textarea class="form-control" name="P8" id="P8" rows="3"></textarea>
-  </div>
-	
-		<div class="form-group">
-    <label for="P9">Cite as principais habilidades que você possui que poderão auxiliá-lo(a) ao longo do curso escolhido.</label>
-    <textarea class="form-control" name="P9" id="P9" rows="3"></textarea>
-  </div>
-	
-  <div class="form-group">
-    <label for="P10">Diga o que você pretende fazer após término do curso técnico.</label>
-    <textarea class="form-control" name="P10" id="P10" rows="3"></textarea>
-  </div>
-	
-	<div class="form-group">
-    <label for="P11">(SOMENTE PARA ENFERMAGEM) O curso de Enfermagem tem estágio obrigatório não remunerado. Você terá disponibilidade de horário no contra turno para realizá-lo?</label>
-    <textarea class="form-control" name="P11" id="P11" rows="3"></textarea>
-  </div>
-	<div class="form-group">
-	<button type="submit" class="btn btn-primary">SALVAR </button>
-		</div>
-</form>	
-
-
-</section>
 <script src="../js/jquery-3.4.1.min.js"></script>
 <script src="../js/popper.min.js"></script>
 <script src="../js/bootstrap-4.4.1.js"></script>
---
+++ entrevista/index_bkp03.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>CEEP</title>
+<link href="../css/bootstrap-4.4.1.css" rel="stylesheet" type="text/css">
+<link href="../css/estilo.css" rel="stylesheet" type="text/css">
+	  <!-- Favicons -->
+<link href="../img/favicon.gif" rel="icon">
+<link href="../img/ceep-touch-icon.jpg" rel="apple-touch-icon">
+</head>
+<body class="container-fluid">
+<header> 
+	
+<!-- Menu Principal -->
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="https://www.ceepcascavel.com.br/"><span class="l1">CEE</span><span class="l2">P</span></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+    <ul class="navbar-nav mr-auto">
+      	<li class="nav-item active"> <a class="nav-link" href="https://www.ceepcascavel.com.br/entrevista/">Nova Entrevista<span class="sr-only">(current)</span></a> </li>
+	  <!--<li class="nav-item active"> <a class="nav-link" href="#">Institucional&nbsp; <span class="sr-only">(current)</span></a> </li>-->
+    </ul>
+  </div>
+</nav>
+<br><br>
+<!-- Fim Menu Principal -->	
+	
+</header>	
+
+	<section>
+  <div class="jumbotron text-center">
+    	<p> <strong>ATENÇÃO!</strong><br></p>
+  		<p>A entrevista on-line é para os candidatos que o curso teve mais de 40 inscritos. De cada curso foram classificados 80 candidatos. Para o curso de <strong>ADMINISTRAÇÃO</strong> do período da <strong>MANHÃ</strong> são 120 classificados. Por que temos 80 vagas, portanto até o candidato 120º deve fazer a entrevista.</p>
+  		<p>Os candidatos classificados devem fazer a entrevista através de um <strong>FORMULÁRIO (via site do CEEP) </strong>. O Formulário On-Line ficará disponível até o dia 05/12/2021 às 23h59 <br>
+  </div>
+	
+		
+   	</div>
+	</div>
+</section>
+<section>
+
+	
+	<form action="salvarEntrevista.php" method="post">
+<div class="form-group">
+    <label for="nome">Nome</label>
+    <input class="form-control" name="nome" id="nome"></input>
+  </div>
+
+  <div class="form-group">
+    <label for="cpf">CPF</label>
+    <input class="form-control" name="cpf" id="cpf"></input>
+  </div>
+
+  <div class="form-group">
+    <label for="curso">Curso</label>
+    <input class="form-control" name="curso" id="curso"></input>
+  </div>
+
+  <div class="form-group">
+    <label for="periodo">Periodo</label>
+    <input class="form-control" name="periodo" id="periodo"></input>
+  </div>
+
+
+<div class="form-group">
+    <label for="P1">Cite sua maior facilidade e a sua maior dificuldade nos estudos e justifique.</label>
+    <textarea class="form-control" name="P1" id="P1" rows="3"></textarea>
+  </div>
+<div class="form-group">
+    <label for="P2">Cite em ordem de preferência três profissões que você deseja exercer no futuro.</label>
+    <textarea class="form-control" name="P2" id="P2" rows="3"></textarea>
+  </div>
+	<div class="form-group">
+    <label for="P3">Cite o principal motivo de escolha deste curso.</label>
+    <textarea class="form-control" name="P3" id="P3" rows="3"></textarea>
+  </div>
+	
+		<div class="form-group">
+    <label for="P4">Comente se há campo de trabalho na sua região para atuação profissional de acordo com o curso escolhido.</label>
+    <textarea class="form-control" name="P4" id="P4" rows="3"></textarea>
+  </div>
+	
+		<div class="form-group">
+    <label for="P5">Cite uma empresa localizada na sua região/cidade que contrata profissionais formados na área do curso que você escolheu.</label>
+    <textarea class="form-control" name="P5" id="P5" rows="3"></textarea>
+  </div>
+	
+		<div class="form-group">
+    <label for="P6">Você já conversou com algum profissional que atua na área do curso que você escolheu para saber como são as atividades desenvolvidas no dia a dia?
+</label>
+    <textarea class="form-control" name="P6" id="P6" rows="3"></textarea>
+  </div>
+	
+		<div class="form-group">
+    <label for="P7">O que você espera do ensino técnico para a sua formação profissional?
+</label>
+    <textarea class="form-control" name="P7" id="P7" rows="3"></textarea>
+  </div>
+	
+	<div class="form-group">
+    <label for="P8">Cite três contribuições que o curso escolhido poderá trazer à sua comunidade/região/cidade.</label>
+    <textarea class="form-control" name="P8" id="P8" rows="3"></textarea>
+  </div>
+	
+		<div class="form-group">
+    <label for="P9">Cite as principais habilidades que você possui que poderão auxiliá-lo(a) ao longo do curso escolhido.</label>
+    <textarea class="form-control" name="P9" id="P9" rows="3"></textarea>
+  </div>
+	
+  <div class="form-group">
+    <label for="P10">Diga o que você pretende fazer após término do curso técnico.</label>
+    <textarea class="form-control" name="P10" id="P10" rows="3"></textarea>
+  </div>
+	
+	<div class="form-group">
+    <label for="P11">(SOMENTE PARA ENFERMAGEM) O curso de Enfermagem tem estágio obrigatório não remunerado. Você terá disponibilidade de horário no contra turno para realizá-lo?</label>
+    <textarea class="form-control" name="P11" id="P11" rows="3"></textarea>
+  </div>
+	<div class="form-group">
+	<button type="submit" class="btn btn-primary">SALVAR </button>
+		</div>
+</form>	
+
+
+</section>
+<script src="../js/jquery-3.4.1.min.js"></script>
+<script src="../js/popper.min.js"></script>
+<script src="../js/bootstrap-4.4.1.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+</body>
+</html>
