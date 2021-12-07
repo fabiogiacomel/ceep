2a9049fd903c8a157a7cc6d32cd9ab9c22d84396
fabiogiacomel <fabioceep@gmail.com>
2021-12-07 20:44:11 -0300

    Update atualizarCPF.php

--- listagem/atualizarCPF.php
+++ listagem/atualizarCPF.php
@@ -29,7 +29,7 @@ function limpar($string){
             try {
                 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $stmt = $conn->prepare("SELECT * FROM tbEntrevistaRespostas");
+                $stmt = $conn->prepare("SELECT * FROM tbEntrevistaRespostas WHERE CPF<>0");
                 $stmt->execute();
 
                 // set the resulting array to associative
