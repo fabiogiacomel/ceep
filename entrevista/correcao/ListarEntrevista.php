81b8ddfb1ab008326084e5cd265ade8ae7bc9222
fabiogiacomel <fabioceep@gmail.com>
2021-12-10 13:08:01 -0300

    Update ListarEntrevista.php

--- entrevista/correcao/ListarEntrevista.php
+++ entrevista/correcao/ListarEntrevista.php
@@ -104,7 +104,7 @@ $vCursos = array('0','Administração','Eletrônica', 'Eletromecânica','Enferma
         
 
         <div class="container mt-3">
-		<form class="form-inline" action="salvarNotaEntrevista.php?curso=<?php $curso ?>& <?php $periodo ?>" method="post">
+		<form class="form-inline" action="salvarNotaEntrevista.php?curso=<?php echo $curso; ?>&periodo<?php echo $periodo; ?>" method="post">
 			<?php echo "<input class=\"form-control\" type=\"text\" value=\"".$idIns."\" id=\"idInsc\" name=\"idIns\" readonly></br>" ?>
             <?php
             echo '<table class="table table-striped">';
