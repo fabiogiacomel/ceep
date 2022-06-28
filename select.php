091a9129df5d3af19b3c99b7db05e4b4a5f3c283
fabiogiacomel <fabioceep@gmail.com>
2022-06-28 14:37:32 -0300

    Update select.php

--- select.php
+++ select.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+  <title>Tabela</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 <body>
 
 <?php
-echo "<table style='border: solid 1px black;'>";
+echo '<table class="table table-dark">';
  echo "<tr><th>ID</th><th>Nome</th><th>Telefone</th><th>Email</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
